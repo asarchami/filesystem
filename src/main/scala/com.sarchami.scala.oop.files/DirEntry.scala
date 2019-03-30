17cfb677239e0ecad2fdeaf667c082bc2293793c
Ali Sarchami <ali.sarchami@gmail.com>
2019-03-30 16:37:48 -0500

    changed path for complex situation

--- src/main/scala/com.sarchami.scala.oop.files/DirEntry.scala
+++ src/main/scala/com.sarchami.scala.oop.files/DirEntry.scala
@@ -3,7 +3,12 @@ package com.sarchami.scala.oop.files
 
 abstract class DirEntry(val parentPath: String, val name: String) {
 
-  def path: String = parentPath + Directory.SEPARATOR + name
+  def path: String = {
+    val separatorIfNecessary =
+      if (Directory.ROOT_PATH.equals(parentPath)) ""
+      else Directory.SEPARATOR
+    parentPath + separatorIfNecessary + name
+  }
 
   def asDirectory: Directory
 
