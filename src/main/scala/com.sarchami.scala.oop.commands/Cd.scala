40a6dd3bf89829a4c3996d4b3413652571f42264
Ali Sarchami <ali.sarchami@gmail.com>
2019-03-30 12:35:29 -0500

    cd command

---
+++ src/main/scala/com.sarchami.scala.oop.commands/Cd.scala
@@ -0,0 +1,55 @@
+package com.sarchami.scala.oop.commands
+
+import com.sarchami.scala.oop.files.{ DirEntry, Directory }
+import com.sarchami.scala.oop.filesystem.State
+import scala.annotation.tailrec
+
+
+class Cd(dir: String) extends Command {
+  override def apply(state: State): State = {
+    /*
+     cd /something/somethingElse/../
+     cd /a/b/c/ = relative
+
+     cd ..
+     cd .
+     cd a/./.././a/
+     */
+    // 1. find root
+    val root = state.root
+    val wd = state.wd
+
+    // 2. find absoloute path of the directory we want to cd to
+    val absoloutePath =
+      if (dir.startsWith(Directory.SEPARATOR)) dir
+      else if (wd.isRoot) wd.path + dir
+      else wd.path + Directory.SEPARATOR + dir
+
+    // 3. find the directory
+    val destinationDirectory = doFindEntry(root, absoloutePath)
+
+    // 4. change the state given the new directory
+    if(destinationDirectory == null || !destinationDirectory.isDirectory)
+      state.setMessage(dir + ": no such directory")
+    else
+      State(root, destinationDirectory.asDirectory)
+  }
+
+  def doFindEntry(root: Directory, path: String): DirEntry = {
+    @tailrec
+    def findEntryHelper(currentDirectory: Directory, path: List[String]): DirEntry = {
+      if (path.isEmpty || path.head.isEmpty) currentDirectory
+      else if (path.tail.isEmpty) currentDirectory.findEntry(path.head)
+      else {
+        val nextDir = currentDirectory.findEntry(path.head)
+        if (nextDir == null || !nextDir.isDirectory) null
+        else findEntryHelper(nextDir.asDirectory, path.tail)
+      }
+    }
+
+    // 1. tokens
+    val tokens: List[String] = path.substring(1).split(Directory.SEPARATOR).toList
+    // 2. navigate to the correcty entry
+    findEntryHelper(root, tokens)
+  }
+}
