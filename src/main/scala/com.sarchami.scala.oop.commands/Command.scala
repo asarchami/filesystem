24f7a74f4dcd85b4e32e1325e26d7f6c4b2ca13c
Ali Sarchami <ali.sarchami@gmail.com>
2019-03-30 12:35:13 -0500

    cd command

--- src/main/scala/com.sarchami.scala.oop.commands/Command.scala
+++ src/main/scala/com.sarchami.scala.oop.commands/Command.scala
@@ -12,6 +12,7 @@ object Command {
   val LS = "ls"
   val PWD = "pwd"
   val TOUCH = "touch"
+  val CD = "cd"
 
   def emptyCommand: Command = new Command {
     override def apply(state: State): State = state
@@ -40,6 +41,10 @@ object Command {
     else if (TOUCH.equals(tokens(0))) {
       if (tokens.length < 2) incompleteCommand(MKDIR)
       else new Touch(tokens(1))
+    }
+    else if (CD.equals(tokens(0))) {
+      if (tokens.length < 2) incompleteCommand(MKDIR)
+      else new Cd(tokens(1))
     }
     else new UnknownCommand
     }
