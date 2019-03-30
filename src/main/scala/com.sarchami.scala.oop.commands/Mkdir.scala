070ef4c2e2faa371654b08b864621f60417f7dbc
Ali Sarchami <ali.sarchami@gmail.com>
2019-03-29 23:05:58 -0500

    removed name since it is already part of the object

--- src/main/scala/com.sarchami.scala.oop.commands/Mkdir.scala
+++ src/main/scala/com.sarchami.scala.oop.commands/Mkdir.scala
@@ -6,6 +6,6 @@ import com.sarchami.scala.oop.filesystem.State
 
 class Mkdir(name: String) extends CreateEntry(name) {
 
-  override def createSpecificEntry(state: State, entryName: String): DirEntry =
-    Directory.empty(state.wd.path, entryName)
+  override def createSpecificEntry(state: State): DirEntry =
+    Directory.empty(state.wd.path, name)
 }
